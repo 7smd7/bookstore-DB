2ef40f32e0bea5c0d2ffb9817ddaa8c3fd384e77
Mohammad <1mo.daryani@gmail.com>
2019-12-28 20:58:42 +0330

    Update create.sql

--- create.sql
+++ create.sql
@@ -28,6 +28,7 @@ DROP FUNCTION IF EXISTS set_rank();
 DROP FUNCTION IF EXISTS is_isbn();
 DROP FUNCTION IF EXISTS add_price();
 DROP FUNCTION IF EXISTS sold_update();
+DROP FUNCTION IF EXISTS get_authors();
 -------------------------------------------------
 DROP VIEW IF EXISTS book_adder;
 DROP VIEW IF EXISTS books_rank;
@@ -515,6 +516,10 @@ CREATE VIEW book_adder AS (
     books.edition,
     books.available_quantity,
     books.price,
+    books.sold_count,
+    books.price * (books.sold_count :: NUMERIC) AS sold_amount,
+    books.avg_rate,
+    array (select names from books_authors ba join get_authors () ga on ba.author_id=ga.id_authors where ba.book_id=books.isbn),
     publishers.name AS publisher,
     authors.id
   FROM books
@@ -533,6 +538,7 @@ CREATE OR REPLACE VIEW books_rank AS (
           FROM books_genres
             JOIN genres ON books_genres.genre_id = genres.id
           WHERE book_id LIKE isbn) AS genres
+
   FROM (SELECT
           books.isbn                   AS isbn,
           title                        AS title,
@@ -543,6 +549,15 @@ CREATE OR REPLACE VIEW books_rank AS (
         GROUP BY books.isbn) AS o
   ORDER BY sold DESC, rate DESC
 );
---
--- SET enable_nestloop=0;
--- SELECT 'postgresql' AS dbms,t.table_catalog,t.table_schema,t.table_name,c.column_name,c.ordinal_position,c.data_type,c.character_maximum_length,n.constraint_type,k2.table_schema,k2.table_name,k2.column_name FROM information_schema.tables t NATURAL LEFT JOIN information_schema.columns c LEFT JOIN(information_schema.key_column_usage k NATURAL JOIN information_schema.table_constraints n NATURAL LEFT JOIN information_schema.referential_constraints r)ON c.table_catalog=k.table_catalog AND c.table_schema=k.table_schema AND c.table_name=k.table_name AND c.column_name=k.column_name LEFT JOIN information_schema.key_column_usage k2 ON k.position_in_unique_constraint=k2.ordinal_position AND r.unique_constraint_catalog=k2.constraint_catalog AND r.unique_constraint_schema=k2.constraint_schema AND r.unique_constraint_name=k2.constraint_name WHERE t.TABLE_TYPE='BASE TABLE' AND t.table_schema NOT IN('information_schema','pg_catalog');
+
+CREATE OR REPLACE FUNCTION get_authors ()
+RETURNS table(id_authors int, names varchar) AS $$
+BEGIN
+   return query
+     SELECT * FROM(
+       SELECT  id , (CONCAT(first_name, ' ', second_name) :: varchar) as name FROM authors where  company_name IS NULL
+       union
+       SELECT id , company_name as name FROM authors where company_name IS NOT NULL
+        ) as q;
+END;
+$$ LANGUAGE plpgsql;
